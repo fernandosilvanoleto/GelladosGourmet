65553e93b54ac5be8d7bf1b9f2098a3298c95b6a
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-01-15 12:55:38 -0300

    Nivelamento entre MVC para aplicações Web com template Engine - Teoria.

--- GelladosGourmet/Program.cs
+++ GelladosGourmet/Program.cs
@@ -20,5 +20,53 @@ namespace GelladosGourmet
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        /*
+         * NIVELAMENTO MVC - 15/01/2020
+         * 
+         * NIVELAMENTO MVC PARA APLICAÇÕES WEB COM TEMPLATE ENGINE
+        
+        Responsabilidades no MVC
+
+        Model
+            estrutura dos dados e suas transformações (domain model)
+            também chamado de "o sistema"
+            composto de ENTITES E SERVICES (relacionados às entities)
+                repositórios (objetos que acessam dados persistentes)
+            coração do negócio
+            regra de negócio
+            inteligência do sistema
+            acesso ao banco de dados
+
+        Controllers
+            receber e tratar as interações do usuário com o sistema
+            receber as ações
+            intermediário
+
+        Views
+            definir a estrutura e comportamento das telas
+            apresentação
+
+        
+        Comunicação entre as camadas ==>
+
+        View (Telas) com:
+            Controlador
+
+        Controlador (Requisições) com:
+            View
+            Model
+
+        Model com:
+            Controlador
+
+        Model dividido em:
+            Service Layer - Entities
+            Data Access Layer - Entities
+
+         */
+
+
+
     }
 }
