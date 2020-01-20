1b4f9a51db192aa11e95113ce5010776e873dfd3
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-01-20 12:29:26 -0300

    Nivelamento sobre Entity Framework.

--- GelladosGourmet/Program.cs
+++ GelladosGourmet/Program.cs
@@ -66,6 +66,51 @@ namespace GelladosGourmet
 
          */
 
+        /*
+         * Nivelamento: Entity Framework - 20/01/2020
+         * 
+         * Problema:
+               Uma grande dificuldade de se criar sistemas orientados a objetos juntamente com a comunicação com o banco de dados relacional!!!
+         
+        Outras Questões:
+            Contexto de persistência (monitorar alterações nos objetos que estão atrelados a uma conexão em um dado momento)
+                - Alterações
+                - Transação
+                - Concorrência
+
+        Mapa de Identidade (cache de objetos já encarregados na memória)
+
+        Carregamento tardio (lazy loading)
+
+        Solução: Mapeamento Objeto-Relacional
+        ORM (Object-Relational Mapping)
+        Permite programar em nível de objetos e comunicar de forma transparente com um banco de dados relacional!!!
+
+        Entity Framework é um ORM!!!
+
+        Duas Principais Classes:
+        DbContext
+            um objeto DbContext encapsula uma sessão com o banco de dados (conectividade) para um determinado modelo de dados (representando por DbSet's)
+                É usado para consultar e salvar entidades no banco de dados
+                Definie quais entidades farão parte do modelo de dados do sistema
+                Pode definir várias configurações
+                É uma combinação dos padrões Unity of Work e Repository
+                    Unity of Work
+                        consistência
+                        mantém uma lista de objetos afetados por uma transação e coordena a escrita de mudanças e trata possíveis problemas de concorrência
+                    Repository
+                        define um objeto capaz de realizar operações de acesso a dados (consultar, salvar, atualizar, deletar) para uma entidade
+
+        DbSet<TEntity>
+            genérico
+            representa a coleção de entidades de um dado tipo em um contexto
+            Tipicamente corresponde a uma tabela do banco de dados
+
+        PROCESSO GERAL PARA SE EXECUTAR OPERAÇÕES
+
+                LINQ ==> DbSet (operações de LINQ e lambda) ==> SQL (conversão em tempo de execução comandos LINQ e lambda em SQL) ==> Banco de Dados (Importante saber)
+
+         */
 
 
     }
