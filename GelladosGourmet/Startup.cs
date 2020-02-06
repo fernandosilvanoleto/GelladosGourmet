e9aca3a1919b6c7c2c169407bc477ca7a090d871
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-02-05 21:16:32 -0300

    Implementando a injenção de dados pela aplicação - Seeding Service.

--- GelladosGourmet/Startup.cs
+++ GelladosGourmet/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using GelladosGourmet.Models;
+using GelladosGourmet.Data;
 
 namespace GelladosGourmet
 {
@@ -39,14 +40,18 @@ namespace GelladosGourmet
             services.AddDbContext<GelladosGourmetContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("GelladosGourmetContext"), builder =>
                     builder.MigrationsAssembly("GelladosGourmet")));
+
+            services.AddScoped<SeedingService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
