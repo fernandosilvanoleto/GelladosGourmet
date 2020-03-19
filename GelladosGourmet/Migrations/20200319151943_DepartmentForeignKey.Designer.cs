ecaaf9be0a21d59fcf4259d1f5b7468f443402a3
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-03-19 12:27:14 -0300

    Implementando cadastro de vendedores - Integridade Referencial - Parte 02.

--- GelladosGourmet/Migrations/20200319151943_DepartmentForeignKey.Designer.cs
+++ GelladosGourmet/Migrations/20200319151943_DepartmentForeignKey.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using GelladosGourmet.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GelladosGourmet.Migrations
 {
     [DbContext(typeof(GelladosGourmetContext))]
-    partial class GelladosGourmetContextModelSnapshot : ModelSnapshot
+    [Migration("20200319151943_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- GelladosGourmet/Services/SellerService.cs
+++ GelladosGourmet/Services/SellerService.cs
@@ -23,6 +23,7 @@ namespace GelladosGourmet.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();// salvar no banco de dados;
         }
